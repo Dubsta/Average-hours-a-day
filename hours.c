3c17e4a644a4375784bd0b1b8c9c868a467512a0
Michael Atkinson <mikeatk88@gmail.com>
2015-03-17 13:32:18 +0900

    Add hours.c

---
+++ hours.c
@@ -0,0 +1,35 @@
+// Given input of the total hours for the year,
+// Outputs the average hours per day.
+
+// Michael Atkinson
+// Mikeatk88@gmail.com
+
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+	// Get total hours for the year
+	printf("Enter total hours for the year: \n");
+	printf("Eg. HH MM\n");
+	int hours, mins;
+	scanf("%d %d", &hours, &mins);
+	printf("You have logged %d hours and %d minutes for the year\n", hours, mins);
+	
+	// Get the time
+	time_t rawtime;
+	time(&rawtime);	
+
+	// Get day of the year from time.h
+	struct tm *now;
+	now = localtime(&rawtime);
+	int yearday = now->tm_yday + 1;
+	printf( "%s", asctime(now) );
+	printf("I've counted %d days for the year.\n", yearday);
+
+	
+
+	// Calc average hours per day
+	
+	return 0;
+}
